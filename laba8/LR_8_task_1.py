1f630fe4bbc5322ed036195b08ef6cf0a0046a90
DNyko <120567760+DNyko@users.noreply.github.com>
2023-12-12 09:56:00 +0000

    laba8

---
+++ laba8/LR_8_task_1.py
@@ -0,0 +1,6 @@
+import cv2
+# LOAD AN IMAGE USING 'IMREAD'
+img = cv2.imread("nykonenko.jpg")
+# DISPLAY
+cv2.imshow("nykonenko",img)
+cv2.waitKey(0)
---
+++ laba8/LR_8_task_3.py
@@ -0,0 +1,24 @@
+import cv2
+
+img = cv2.imread("nykonenko.jpg")
+
+# Завантаження передньо навченого класифікатора виявлення обличчя
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Перетворення зображення в градації сірого для виявлення обличчя
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Виконання виявлення обличчя
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+# Перевірка, чи виявлено обличчя
+if len(faces) > 0:
+    # Припускаючи, що хочемо вирізати перше виявлене обличчя
+    x, y, w, h = faces[0]
+    imgCropped = img[y:y + h, x:x + w]
+
+    # Показ вирізаного обличчя
+    cv2.imshow("Image Cropped", imgCropped)
+    cv2.waitKey(0)
+else:
+    print("Обличчя не виявлено.")
